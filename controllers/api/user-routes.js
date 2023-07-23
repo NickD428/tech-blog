489effe78a3e33d327749b731c946568b8608dc4
Nick <chargerfan428@aol.com>
2023-07-23 13:35:18 -0700

    fixed bugs for login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -117,10 +117,7 @@ router.post('/login', (req, res) => {
                 req.session.username = dbUserData.username;
                 req.session.loggedIn = true;
 
-                res.json({
-                    user: dbUserData,
-                    message: 'You are now logged in!'
-                });
+               ;
             });
         });
 });
